db2a113fc9ccc56a4392c3ef9636c4a3b77290fc
Arnit Sinha <75295458+arnitsinha@users.noreply.github.com>
2024-02-10 15:10:11 -0500

    Added all files.

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const LINKS = {
+  github: "https://github.com/arnitsinha",
+  linkedin: "https://www.linkedin.com/in/arnitsinha/",
+  medium: "",
+  discord: "https://discordapp.com/users/163300027618295808",
+  dribble: "",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
---
+++ src/lib/helpers.ts
@@ -0,0 +1,28 @@
+export function getCurrentTimeInEST(): Date {
+  // Create a date object with the current UTC time
+  const now = new Date();
+
+  // Convert the UTC time to EST (UTC-5)
+  const offsetEST = -5;
+  now.setHours(now.getUTCHours() + offsetEST);
+
+  return now;
+}
+
+export function formatTimeForEST(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true, // This will format the time in 12-hour format with AM/PM
+    timeZone: "America/New_York", // Set to Eastern Time Zone
+  };
+
+  let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
+
+  // Append the time zone abbreviation. 
+  // For simplicity, here I'm just appending "EST".
+  formattedTime += " EST";
+
+  return formattedTime;
+}
