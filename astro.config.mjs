699e27ad685b5ffcbe8049057b12094fb463b6dd
Arnit Sinha <75295458+arnitsinha@users.noreply.github.com>
2024-06-09 19:50:00 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,33 @@ import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
 import partytown from '@astrojs/partytown';
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://arnitsinha.com",
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://arnitsinha.com/sitemap-index.xml",
+        "https://arnitsinha.com/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+    icon(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+  output: "server",
+  adapter: netlify(),
   site: 'https://arnitsinha.com',
 });
